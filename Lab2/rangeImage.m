16c2e8f13d1d6a742bb2e50692faa7e3668da78a
Asra <asram363@gmail.com>
2017-09-05 11:22:39 -0400

    lab2 folder and rangeimage class

---
+++ Lab2/rangeImage.m
@@ -0,0 +1,11 @@
+classdef rangeImage
+
+    methods
+        function [ x y th] = irToXy( i, r )
+        % irToXy finds position and bearing of a range pixel endpoint
+        % Finds the position and bearing of the endpoint of a range pixel in
+        % the plane.
+         % Fill in code here
+        end
+    end
+end
