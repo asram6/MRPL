951bc791ac328ca7ac5c06abdcdebf3b853a7ebd
Asra <asram363@gmail.com>
2017-09-10 17:19:54 -0400

    lab 2

--- Lab2/rangeImage.m
+++ Lab2/rangeImage.m
@@ -14,30 +14,39 @@ classdef rangeImage
             y = r*sin(th);
         end
         
-        function [] = followStraight(robot)
+        function [] = follow(robot)
             idealObjectRange = 0.5;
-            gain = .3;
+            gain = .6;
+            figure(1);
+            clf;
             while true
-                [x, y, ~] = rangeImage.findNearest(robot);
-                dist = sqrt(x.^2 + y.^2);
+                [x, y, th, dist] = rangeImage.findNearest(robot);
+                %dist = sqrt(x.^2 + y.^2);
                 v = (dist-idealObjectRange)*gain;
-                if ((dist == 0) || (abs(dist-idealObjectRange) < .07))
+                if ((dist == 1.0) || (abs(dist-idealObjectRange) < .03))
                     v = 0;
                 end
-                robot.sendVelocity(v, v);
-                pause(0.3);
+                vr = v + .0425*(th);
+                vl = v - .0425*(th);
+                robot.sendVelocity(vr, vl);
+                pause(0.5);
+                plot(-y, x, 'x');
+                axis([-2 2 -2 2]);
+                title('Closest point in robot frame');
+                xlabel('X (m)');
+                ylabel('Y (m)');
             end
         end
         
-        function [x, y, th] = findNearest(robot)
+        function [x, y, th, minDist] = findNearest(robot)
            
            ranges = robot.laser.LatestMessage.Ranges;
            minDist = 1.0;
            x = 0;
            y = 0;
-           th = 1;
+           th = 0;
            for i = 1:360 
-               if ((ranges(i) < minDist) && (ranges(i) > 0.06))
+               if ((ranges(i) < minDist) && (ranges(i) > 0.1))
                    [x1, y1, th1] = rangeImage.irToXy(i, ranges(i));
                    if (abs(th1) < (pi/2))
                        x = x1;
@@ -47,19 +56,13 @@ classdef rangeImage
                    end
                end
            end
-           plot(-1:1,-1:1);
-           hold on
-           plot(x, y, 'x');
            
         end
         function [] = test()
             robot = raspbot('Raspbot-09');
             robot.startLaser();
             pause(4);
-            %while true
-            rangeImage.followStraight(robot);
-            %    pause(0.2);
-            %end
+            rangeImage.follow(robot);
             robot.stopLaser();
         end
     end
