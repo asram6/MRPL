6e0da08a76149e49d63003d8b861a792fe5a0b2d
Asra <asram363@gmail.com>
2017-09-16 15:42:27 -0400

    exercise1/2

--- Lab3/encoderEventListener.m
+++ Lab3/encoderEventListener.m
@@ -1,3 +1,34 @@
+<<<<<<< HEAD
+function encoderEventListener()
+    %EncoderEventListener Invoked when new encoder data arrives. 
+    % A MATLAB event listener for the Robot. Invoked when
+    % encoder data arrives.
+    fprintf("HERE");
+    fprintf('%d', 1);
+    persistent tPrev;
+    persistent encoderPrev;
+    persistent robot;
+    if isempty(tPrev) 
+        fprintf("in tprev")
+        tPrev = tic;
+    end
+    if isempty(robot)
+        fprintf("in robot")
+        robot = raspbot('Raspbot-09');
+    end
+    if isempty(encoderPrev)
+        fprintf("in encoderpev")
+        encoderPrev = (robot.encoders.LatestMessage.Vector.X + robot.encoders.LatestMessage.Vector.Y)/2;
+    end
+    tDiff = toc(tPrev);
+    x = robot.encoders.LatestMessage.Vector.X;
+    y = robot.encoders.LatestMessage.Vector.Y;
+    encoderNow = (x+y)/2;
+    encoderDiff = encoderNow - encoderPrev;
+    velocity = encoderDiff/tDiff;
+    plot(tDiff, velocity);
+    
+=======
 function encoderEventListener(handle, event)
     %EncoderEventListener Invoked when new encoder data arrives. 
     % A MATLAB event listener for the Robot. Invoked when
@@ -6,4 +37,5 @@ function encoderEventListener(handle, event)
     fprintf('%d', 1);
     fprintf('%d', handle);
     fprintf('%d', event);
+>>>>>>> a2790b87750cd09b8b8c8463a4a183dad957e81d
 end
--- Lab3/exercise1.m
+++ Lab3/exercise1.m
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+robot = raspbot('Raspbot-09');
+robot.sendVelocity(.050, .050);
+robot.encoders.NewMessageFcn = @encoderEventListener;
+=======
 robot = raspbot('Raspbot-09');
 robot.sendVelocity(.050, .050);
 robot.startLaser();
@@ -5,3 +10,4 @@ robot.encoders.NewMessageFcn = @encoderEventListener;
 pause(10)
 robot.sendVelocity(0, 0);
 robot.shutdown();
+>>>>>>> a2790b87750cd09b8b8c8463a4a183dad957e81d
