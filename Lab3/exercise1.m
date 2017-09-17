6e7612c8e33e9b70819b38457b89cb2cca572050
rupalnahar <rnahar@andrew.cmu.edu>
2017-09-17 16:19:13 -0400

    figure8 attempt

--- Lab3/exercise1.m
+++ Lab3/exercise1.m
@@ -1,8 +1,18 @@
-robot = raspbot('Raspbot-09');
+global robot
+robot = raspbot('sim');
+pause(2);
 robot.encoders.NewMessageFcn = @encoderEventListener;
+pause(2);
+tstart = tic;
 while true
     robot.sendVelocity(.05, .05);
-    pause(0.05);
+    pause(0.1);
 end
+robot.sendVelocity(0, 0);
+robot.encoders.NewMessageFcn = [];
+pause(0.05);
 
-
+function encoderEventListener(handle,event)
+    global robot
+    exercise3.exercise2(robot);
+end
