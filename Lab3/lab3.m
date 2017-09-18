e94a7255ee7698057dfb56034a7d16cfa3b827f0
Asra <asram363@gmail.com>
2017-09-18 19:43:12 -0400

    figure 8

--- Lab3/lab3.m
+++ Lab3/lab3.m
@@ -3,6 +3,8 @@
    global currval;
    preval = false;
    currval = false;
+   robot = raspbot("Raspbot-9");
+   pause(3);
    vt = 0.2; sf = 1; tf = sf/vt;
    ktheta = 2*pi/sf; kk = 15.1084;
    ks = 3; Tf = ks * tf;
@@ -10,21 +12,20 @@
    t = toc(tstart);
    vlArr = []; vrArr = []; dtArr = [];
    xArr = []; yArr = [];
-   robot = raspbot("Raspbot-9");
    fprintf("105\n");
    robot.encoders.NewMessageFcn = @encoderEventListener;
+
+   prevx = robot.encoders.LatestMessage.Vector.X;
+   prevy = robot.encoders.LatestMessage.Vector.Y;
    x = 0;
    y = 0;
    th = 0;
    while(t < Tf) 
-       fprintf("in loop\n");
        while (preval == currval)
            pause(0.001);
        end
-       fprintf("out of loop\n");
        preval = currval;
-       encoderx = robot.encoders.LatestMessage.Vector.X;
-       encodery = robot.encoders.LatestMessage.Vector.Y;
+       
        st = vt*t/ks;
        angle = ktheta*st;
        while (angle > pi)
@@ -33,33 +34,46 @@
        while (angle < -1 * pi)
            angle = angle + 2*pi;  
        end
+       angle = angle; %error scaling
        k = (kk/ks)*sin(angle);
        omegat = k*vt;
-       vr = vt + (0.0425) * omegat;
-       vl = vt - (0.0425) * omegat;
+       vr = vt + (0.044) * omegat;
+       vl = vt - (0.044) * omegat;
        vlArr = [vlArr, vl];
        vrArr = [vrArr, vr];
        robot.sendVelocity(vl, vr);
        plot(yArr, xArr);
        T = toc(tstart);
+       encoderx = robot.encoders.LatestMessage.Vector.X;
+       encodery = robot.encoders.LatestMessage.Vector.Y;
+       diffx = encoderx - prevx;
+       diffy = encodery - prevy;
+       prevx = encoderx;
+       prevy = encodery;
        dt = (T - t);
-       th = th + omegat*dt;
-       x = x + vt*cos(th)*dt;
-       y = y + vt*sin(th)*dt;
+       vlactual = diffx/dt;
+       vractual = diffy/dt;
+       Vactual = (vlactual+vractual)/2;
+       omegaActual = (vractual-vlactual)/.088;
+       th = th + omegaActual*dt;
+       x = x + Vactual*cos(th)*dt;
+       y = y + Vactual*sin(th)*dt;
        xArr = [xArr, x];
        yArr = [yArr, y];
        plot(xArr, yArr);
+       title("Robot Encoder Data Moving in Figure 8");
+       xlabel('X(m)');
+       ylabel('Y(m)');
        t = T;
        pause(0.1);
-       fprintf("%d %d\n", vl, vr);
    end
    robot.encoders.NewMessageFcn = [];
+   robot.sendVelocity(0,0);
    fprintf("%d \n", length(vlArr));
    %exercise3.modelDiffSteerRobot(vlArr, vrArr, 0, Tf, dtArr); 
   
  
  function encoderEventListener(handle,event)
-           fprintf("HERE\n");
            global currval;
            currval = ~currval;
            encoderDataTimestamp = double(event.Header.Stamp.Sec);
