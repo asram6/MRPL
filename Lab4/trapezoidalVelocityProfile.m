c7c0ffd8ba90b438c1fcdaa94de8b2f63a7af48c
rupalnahar <rnahar@andrew.cmu.edu>
2017-09-25 21:09:34 -0400

    lab 4 full and final (hopefully)

--- Lab4/trapezoidalVelocityProfile.m
+++ Lab4/trapezoidalVelocityProfile.m
@@ -2,14 +2,21 @@ function uref  = trapezoidalVelocityProfile( t , amax, vmax, dist, sgn)
     tramp = vmax/amax;
     sf = dist;
     tf = (sf + vmax^2/amax)/vmax;
-    if (t < tramp)
+    if (t < 0)
+        uref = 0;
+    elseif (t < tramp)
         uref = amax * t;
-    elseif ((tf - t) < tramp)
-        uref = -1 * amax * (tf - t);
     elseif ((t > tramp) && (t < tf - tramp))
         uref = vmax;
+    elseif (t > tf)
+        uref = 0;
+    elseif ((tf - t) < tramp)
+        uref = amax * (tf - t);
     else 
         uref = 0;
+    end
+    if (uref < -1 * 0.25)
+        fprintf("!!!! t = %d, tramp = %d, tf = %d",t,tramp,tf);
     end
     uref = sgn * uref;
 end
